42b67dc3dedd644e7eb0d4e9c14a0a8f169ea981
htilssu <tolashuu@gmail.com>
2024-11-27 04:27:01 +0700

    update using oak

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
-import express from 'express';
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-const app = express();
-console.log(123)
+
+const app = new Application();
+const router = new Router();
+
+router.get("/api", (ctx) => {
+    ctx.response.body = { message: "This is a simple API in Deno" };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Server is running on http://localhost:8000");
+await app.listen({ port: 8000 });
